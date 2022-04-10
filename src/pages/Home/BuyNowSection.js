5d434b30d9bbeb0cdb594a0cc290dd18845f9131
JohnnyNi <68974700+johnnyni2961@users.noreply.github.com>
2022-04-09 18:00:28 -0700

    Update BuyNowSection.js

--- src/pages/Home/BuyNowSection.js
+++ src/pages/Home/BuyNowSection.js
@@ -236,16 +236,16 @@ const BuyNowSection = () => {
                 spacing={7}
                 style={{ paddingTop: '8px' }}
               >
-                <Grid item xs={12} sm={6} className={classes.gameAvatar}>
+                <Grid item xs={12} sm={6} justifyContent="center" className={classes.gameAvatar}>
                   <img src={gameAvatar1} />
                 </Grid>
-                <Grid item xs={12} sm={6} className={classes.gameAvatar}>
+                <Grid item xs={12} sm={6} justifyContent="center" className={classes.gameAvatar}>
                   <img src={gameAvatar2} />
                 </Grid>
-                <Grid item xs={12} sm={6} className={classes.gameAvatar}>
+                <Grid item xs={12} sm={6} justifyContent="center" className={classes.gameAvatar}>
                   <img src={gameAvatar3} />
                 </Grid>
-                <Grid item xs={12} sm={6} className={classes.gameAvatar}>
+                <Grid item xs={12} sm={6} justifyContent="center" className={classes.gameAvatar}>
                   <img src={gameAvatar4} />
                 </Grid>
               </Grid>
